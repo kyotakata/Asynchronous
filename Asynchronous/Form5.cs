ba97eac1b888390359dd071950f8e9b932c96517
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-21 06:39:37 +0900

    async await編

---
+++ Asynchronous/Form5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)// awaitがある関数はasyncが必要
+        {
+            dataGridView1.DataSource = await Task.Run(() => GetData());// awaitを入れることでTask.Runが終わるまで次の処理に進まない
+            MessageBox.Show("完了");
+        }
+
+        /// <summary>
+        /// ワーカスレッドで実行する非同期な処理
+        /// </summary>
+        private List<DTO> GetData()// ThreadPoolとは違って、object以外の型を指定できる
+        {
+            //var dto = o as DTO;
+            //if (dto == null) { return; }
+
+            var result = new List<DTO>();
+            for (int i = 0; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                result.Add(new DTO(i.ToString(), "Name" + i));
+            }
+
+            return result;
+        }
+    }
+}
