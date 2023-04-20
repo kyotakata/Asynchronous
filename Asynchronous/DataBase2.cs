e1f8f8cbc787b90d37b7cdf23395b403c7139fbe
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-21 07:31:03 +0900

    10.例外とキャンセルトークンを使ってキャンセルする方法【推奨】

---
+++ Asynchronous/DataBase2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    internal class DataBase2
+    {
+        /// <summary>
+        /// ワーカスレッドで実行する非同期な処理
+        /// </summary>
+        internal List<DTO> GetData(System.Threading.CancellationToken token)// ThreadPoolとは違って、object以外の型を指定できる
+        {
+
+            //var dto = o as DTO;
+            //if (dto == null) { return; }
+
+            var result = new List<DTO>();
+            for (int i = 0; i < 5; i++)
+            {
+                //トークンでキャンセルされるのを睨んでいる。
+                token.ThrowIfCancellationRequested();// このトークンに対して取り消しが要求された場合、OperationCanceledExceptionをスローします。
+
+                System.Threading.Thread.Sleep(1000);
+                result.Add(new DTO(i.ToString(), "Name" + i));
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Asynchronous/Form8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous
+{
+    public partial class Form8 : Form
+    {
+        System.Threading.CancellationTokenSource _token;
+        private DataBase2 _dataBase = new DataBase2();
+
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)// awaitがある関数はasyncが必要
+        {
+            try
+            {
+                _token = new System.Threading.CancellationTokenSource();// ボタン押されたときにnew
+                dataGridView1.DataSource = await Task.Run(// awaitを入れることでTask.Runが終わるまで次の処理に進まない
+                    () => _dataBase.GetData(_token.Token), _token.Token);// 第二引数でトークンを投げておくと、既にキャンセルされていた場合にTask.Runが動かずにすぐに止まるようになる。
+                MessageBox.Show("完了");
+            }
+            catch (OperationCanceledException o)
+            {
+                MessageBox.Show("キャンセルされました");
+            }
+            finally
+            {
+                _token.Dispose();
+                _token = null;
+            }
+
+        }
+
+
+        private void CancelButtun_Click(object sender, EventArgs e)
+        {
+            _token?.Cancel();
+        }
+    }
+}
