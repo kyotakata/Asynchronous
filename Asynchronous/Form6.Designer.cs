ed52455e6c64da16970ce4226c1e31235a4fe285
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-21 06:48:37 +0900

    8.キャンセルする方法：フラグ編【非推奨パターン】

---
+++ Asynchronous/Form6.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Asynchronous
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CancelButtun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(117, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // CancelButtun
+            // 
+            this.CancelButtun.Location = new System.Drawing.Point(12, 32);
+            this.CancelButtun.Name = "CancelButtun";
+            this.CancelButtun.Size = new System.Drawing.Size(75, 23);
+            this.CancelButtun.TabIndex = 3;
+            this.CancelButtun.Text = "Cancel";
+            this.CancelButtun.UseVisualStyleBackColor = true;
+            this.CancelButtun.Click += new System.EventHandler(this.CancelButtun_Click);
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.CancelButtun);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form6";
+            this.Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button CancelButtun;
+    }
+}
+
---
+++ Asynchronous/Form6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous
+{
+    public partial class Form6 : Form
+    {
+        private bool _isCancel = false;
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)// awaitがある関数はasyncが必要
+        {
+            _isCancel= false;
+            dataGridView1.DataSource = await Task.Run(() => GetData());// awaitを入れることでTask.Runが終わるまで次の処理に進まない
+
+            if (_isCancel)
+            {
+                MessageBox.Show("キャンセルされました");
+            }
+
+            MessageBox.Show("完了");
+        }
+
+        /// <summary>
+        /// ワーカスレッドで実行する非同期な処理
+        /// </summary>
+        private List<DTO> GetData()// ThreadPoolとは違って、object以外の型を指定できる
+        {
+            //var dto = o as DTO;
+            //if (dto == null) { return; }
+
+            var result = new List<DTO>();
+            for (int i = 0; i < 5; i++)
+            {
+                if (_isCancel)
+                {
+                    return null;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+                result.Add(new DTO(i.ToString(), "Name" + i));
+            }
+
+            return result;
+        }
+
+        private void CancelButtun_Click(object sender, EventArgs e)
+        {
+            _isCancel = true;
+        }
+    }
+}
