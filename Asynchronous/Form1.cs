32cb99abdf078f8f3ed87c205eeb76c3ea43ebe8
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-20 20:45:44 +0900

    プロジェクト ファイルを追加します。

---
+++ Asynchronous/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetData();
+        }
+
+        private List<DTO> GetData()
+        {
+            var result = new List<DTO>();
+            for (int i = 0; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                result.Add(new DTO(i.ToString(), "Name" + i));
+            }
+
+            return result;
+        }
+    }
+}
